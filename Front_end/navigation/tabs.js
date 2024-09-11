902fce93ab6a4675e80bda6c95620673e2ad4d07
Silverss03 <nguyenhuumanhhp2003@gmail.com>
2024-09-11 11:32:49 +0700

    add bottom menu icons

--- Front_end/navigation/tabs.js
+++ Front_end/navigation/tabs.js
@@ -6,18 +6,58 @@ import Cart from '../screens/Cart';
 import Account from '../screens/Account';
 import Login from '../screens/Login'
 import Register from '../screens/Register'
+import { House } from 'lucide-react-native';
+import { Pizza } from 'lucide-react-native';
+import { TicketPercent } from 'lucide-react-native';
+import { ShoppingCart } from 'lucide-react-native';
+import { CircleUser } from 'lucide-react-native';
 
 const Tab = createBottomTabNavigator();
 const Tabs = () => {
   return (
 	<Tab.Navigator screenOptions={{headerShown : false}}>
-		<Tab.Screen name = "Trang Chủ" component={Home} />
-		<Tab.Screen name = "Thực Đơn" component={Menu}/>
-		<Tab.Screen name = "Khuyến Mãi" component={Prefentials} />
-		<Tab.Screen name = "Đơn Hàng" component={Cart} />
-		<Tab.Screen name = "Tài Khoản" component={Account} />
-		<Tab.Screen name = "Login" component={Login} options={{ tabBarButton: () => null }}/>
-		<Tab.Screen name = "Register" component={Register} options={{ tabBarButton: () => null }}/>
+		<Tab.Screen 
+			name = "Trang Chủ" 
+			component={Home} 
+			options={{
+			tabBarIcon: ({ color, size }) => (
+				<House color={color} size={size}/>
+			),
+			}}  />
+		<Tab.Screen 
+			name = "Thực Đơn" 
+			component={Menu}
+			options={{
+				tabBarIcon: ({ color, size }) => (
+					<Pizza color={color} size={size}/>
+			),
+			}}  />
+		<Tab.Screen 
+			name = "Khuyến Mãi" 
+			component={Prefentials} 
+			options={{
+				tabBarIcon: ({ color, size }) => (
+					<TicketPercent color={color} size={size}/>
+			),
+			}}  />
+		<Tab.Screen 
+			name = "Đơn Hàng" 
+			component={Cart} 
+			options={{
+				tabBarIcon: ({ color, size }) => (
+					<ShoppingCart color={color} size={size}/>
+			),
+			}} />
+		<Tab.Screen 
+			name = "Tài Khoản" 
+			component={Account}
+			options={{
+				tabBarIcon: ({ color, size }) => (
+					<CircleUser color={color} size={size}/>
+			),
+			}}  />
+		<Tab.Screen name = "Đăng nhập" component={Login} options={{ tabBarButton: () => null }}/>
+		<Tab.Screen name = "Đăng ký" component={Register} options={{ tabBarButton: () => null }}/>
 	</Tab.Navigator>
 	);
 }
