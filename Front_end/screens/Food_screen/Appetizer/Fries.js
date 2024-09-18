b6bd43367f173dcf7bbbaa5a2cb412d39ea92420
Silverss03 <nguyenhuumanhhp2003@gmail.com>
2024-09-18 16:34:14 +0700

    Update some food screens

--- Front_end/screens/Food_screen/Appetizer/Fries.js
+++ Front_end/screens/Food_screen/Appetizer/Fries.js
@@ -1,55 +1,26 @@
 import {View, Text, FlatList, Image, SafeAreaView, TouchableOpacity, Dimensions} from 'react-native'
-const screenWidth = Dimensions.get('window').width ;
-const items = [
-    {
-        id : 1,
-        img : require('../../../assets/Menu_img/Appetizer_img/muc_chien_gion.jpg'),
-        name : 'Mực Chiên Giòn',
-        des : `Mực tẩm bột chiên giòn dùng kèm sốt ngò tây`,
-        price : '129.000đ'
-    },
-    {
-        id : 2,
-        img : require('../../../assets/Menu_img/Appetizer_img/gio_khoai_chien.jpg'),
-        name : 'Giỏ Khoai Tây Chiên',
-        des : 'Sự kết hợp của nhiều kiểu chế biến khoai tây',
-        price : '89.000đ'
-    },
-    {
-        id : 3,
-        img : require('../../../assets/Menu_img/Appetizer_img/khoai_tay_chien.jpg'),
-        name : 'Khoai Tây Chiên',
-        des : 'Khoai tây sợi được chiên và tẩm một lớp muối thấm vị',
-        price : '79.000đ'
-    },
-];
-
-const renderItem = ({item}) => {
-    return (
-        <TouchableOpacity>
-            <View style={{flexDirection: 'row', textAlign: 'left', fontSize: 15, backgroundColor: 'white', width: screenWidth, marginBottom : 16}}>
-                <Image style={{width: 170, height: 170, marginLeft : 2}} source={item.img} />
-                <View style={{margin: 12, flex: 1}}>
-                    <Text style={{marginBottom: 4, fontSize: 20}}>{item.name}</Text>
-                    <View>
-                        <Text style={{marginBottom: 60, fontSize: 16, flexShrink: 1, width: '100%'}}>{item.des}</Text>
-                    </View>
-                    <Text style={{fontSize: 18}}>{item.price}</Text>
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
-};
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Food from '../../../components/Food';
 
 const Fries = () =>{
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        // Fetch data from the backend
+        const fetchData = async () => {
+            try {
+              const response = await axios.get('food/fries');
+              setItems(response.data);
+            } catch (error) {
+              console.error('Error fetching data:', error);
+            }
+          };
+      
+          fetchData();
+    }, []);
+
     return(
-        <SafeAreaView >
-            <FlatList data = {items}
-                    keyExtractor = {(item) => item.id}
-                    renderItem = {renderItem}>
-                    
-            </FlatList>
-        </SafeAreaView>
+        <Food foodItems={items} generateKey={(item) => item.id}/>
     )
 }
 
--- Front_end/screens/Food_screen/Pizza/SeaPizza.js
+++ Front_end/screens/Food_screen/Pizza/SeaPizza.js
@@ -1,7 +1,7 @@
 import {View, Text, FlatList, Image, SafeAreaView, TouchableOpacity, Dimensions} from 'react-native'
 import axios from 'axios' 
-import React, { useEffect, useState } from 'react';
-const screenWidth = Dimensions.get('window').width ;
+import React, { useEffect, useState } from 'react'
+import Food from '../../../components/Food'
 
 const SeaPizza = () =>{
     const [items, setItems] = useState([]);
@@ -9,7 +9,7 @@ const SeaPizza = () =>{
         // Fetch data from the backend
         const fetchData = async () => {
             try {
-              const response = await axios.get('http://10.0.2.2:3000/api/food/sea_pizza');
+              const response = await axios.get('food/sea_pizza');
               setItems(response.data);
             } catch (error) {
               console.error('Error fetching data:', error);
@@ -19,32 +19,8 @@ const SeaPizza = () =>{
           fetchData();
     }, []);
     
-    const renderItem = ({item}) => {
-        const imageUrl = `http://10.0.2.2:3000${item.img}`;
-        return (
-            <TouchableOpacity>
-                <View style={{flexDirection: 'row', textAlign: 'left', fontSize: 15, backgroundColor: 'white', width: screenWidth, marginBottom : 16}}>
-                    <Image style={{width: 170, height: 170}} source={{uri : imageUrl}} />
-                    <View style={{margin: 12, flex: 1}}>
-                        <Text style={{marginBottom: 4, fontSize: 20}}>{item.name}</Text>
-                        <View>
-                            <Text style={{marginBottom: 60, fontSize: 16, flexShrink: 1, width: '100%'}}>{item.des}</Text>
-                        </View>
-                        <Text style={{fontSize: 18}}>{item.price}đ</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-        );
-    };
     return(
-        <SafeAreaView >
-            <FlatList data = {items}
-                    keyExtractor = {(item) => item.id}
-                    renderItem = {renderItem}
-                    initialNumToRender={5}>
-                    
-            </FlatList>
-        </SafeAreaView>
+        <Food foodItems={items} generateKey={(item) => item.id}/>
     )
 }
 
