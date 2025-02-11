f61e684aac6e14b97c0ee8088d933776e20f2396
Silverss03 <nguyenhuumanhhp2003@gmail.com>
2025-02-11 10:56:44 +0700

    update db info

--- Back_end/database/database.js
+++ Back_end/database/database.js
@@ -1,12 +1,14 @@
 // Get the client
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    port: '3308',
-    user: 'root',
-    password: '0915166497Bc#',
-    database : 'pizzashop',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit : 0
